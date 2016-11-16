504a8ff89fcd348648d7ca12726e8800e7192412
toC1421 <s1421030@gmail.com>
2016-11-16 18:02:51 +0000

    fix typo

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -2,6 +2,16 @@
 
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
+chrome.contextMenus.create({
+  title : 'プチブックマークに飛ぶ',
+  type  : 'normal',
+  onclick : function(info){
+    var url = 'https://www.google.co.jp/'
+    chrome.tabs.create({ url : url});
+  }
+});
+    
+    
 });
 
 console.log('\'Allo \'Allo! Event Page');
